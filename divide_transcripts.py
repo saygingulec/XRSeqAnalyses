3e4022c38fe38d08dd1ab20bacb441a41fedfab8
saygingulec <50466176+saygingulec@users.noreply.github.com>
2020-07-08 22:25:32 -0400

    Update divide_transcripts.py

--- divide_transcripts.py
+++ divide_transcripts.py
@@ -1,22 +1,17 @@
 """
-Divides transcripts into 100 bins and adds up- and downstream sequences divided into 20 bins that are
-sized similarly to the transcript bins.
+Divides transcripts into 100 bins and add 2 kb of up- and downstream sequences divided into 80 bp long bins.
 
-Used the code in the block comments instead if you want to add 2 kb of up- and downstream sequences divided into
-100 bp long bins.
+Used the code in the block comments instead if you want to add up- and downstream sequences divided into 20 bins
+that are sized similarly to the transcript bins.
 
 input row
 chr2L	7528	9484	+	CG11023	NM_001272857.1	1617
 
 output rows from one transcript
-chr2L	7158	7177	+	CG11023	NM_001272857.1	1617
-chr2L	7177	7197	+	CG11023	NM_001272857.1	1617
 ...
 chr2L	7528	7547	+	CG11023	NM_001272857.1	1617
 chr2L	7547	7567	+	CG11023	NM_001272857.1	1617
 ...
-chr2L	9835	9854	+	CG11023	NM_001272857.1	1617
-chr2L	9854	9873	+	CG11023	NM_001272857.1	1617
 """
 
 transcript_list = "transcript_list.bed"
@@ -34,11 +29,10 @@ with open(transcript_list) as f:
         starts = []
         ends = []
 
-        """
-        # make 20 bins of 100 bp long upstream
-        for i in range(20):
-        starts.append(str(upstart + i*100))
-        """
+        # make 25 bins of 80 bp long upstream
+        for i in range(25):
+        starts.append(str(upstart + i*80))
+
         # divide the genes into 100 bins
         prev = start
         for i in range(100):
@@ -62,6 +56,7 @@ with open(transcript_list) as f:
                 llen = int(txlen / 100)
                 prev = prev + llen
 
+        """
         # insert 20 bins of upstream sized similarly to the transcript bins
         prev = start - int(txlen / 100)
         for i in range(20):
@@ -107,16 +102,16 @@ with open(transcript_list) as f:
                 starts.append(str(prev))
                 llen = int(txlen / 100)
                 prev = prev + llen
+        """
 
-        """
-        # make 20 bins of 100 bp long downstream
-        for i in range(20):
-            starts.append(str(end + i * 100))
-        """
+        # make 25 bins of 80 bp long downstream
+        for i in range(25):
+            starts.append(str(end + i * 80))
 
         ends = starts[1:]
-        endend = int(starts[-1]) + int(txlen / 100)
-        ends.append(str(endend))     # ends.append(str(downend)) for 20 bins of 100 bp long downstream
+        # endend = int(starts[-1]) + int(txlen / 100)
+        # ends.append(str(endend)) for similarly sized up-down stream
+        ends.append(str(downend))
 
         with open(output, "a") as d:
             for i in range(len(starts)):
