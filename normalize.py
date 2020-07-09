27560a0a8761c12a070a8084d2d1fa6eac10f079
saygingulec <50466176+saygingulec@users.noreply.github.com>
2020-07-09 00:43:34 -0400

    Update normalize.py

--- normalize.py
+++ normalize.py
@@ -1,5 +1,5 @@
 """
-Normalizes the read counts using RKPM.
+Normalizes the read counts using RPBM.
 """
 
 sample_list = [
@@ -9,7 +9,7 @@ sample_list = [
     "Experiment2_Sample2_trimmed_sorted_TS.txt"
 ]
 
-read_counts = [5000000, 6000000, 10000000, 8000000]
+read_counts = [1000000]
 
 normalization_factors = []
 for i in read_counts:
@@ -22,7 +22,11 @@ for i in range(len(sample_list)):
         normalized = sample_list[i][:-4] + "_normalized.txt"
         for line in f:
             line = line.strip().split()
-            line[7] = str( (int(line[7]) * normalization_factors[i]) / ( int(line[2]) - int(line[1]) ) )
+            read_count = int(line[7])
+            bin_start = int(line[1])
+            bin_end = int(line[2])
+            bin_length = bin_end - bin_start
+            line[7] = str((read_count * normalization_factors[i]) / bin_length)
             line = "\t".join(line) + "\n"
             with open(normalized, "a") as d:
                 d.write(line)
