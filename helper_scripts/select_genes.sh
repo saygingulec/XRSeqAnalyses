5b9262d7d3397f244aed2a12fe6e4472f3e2c309
saygingulec <50466176+saygingulec@users.noreply.github.com>
2021-06-05 11:05:31 -0400

    Add files via upload

--- helper_scripts/select_genes.sh
+++ helper_scripts/select_genes.sh
@@ -1,8 +1,9 @@
 GENELIST_NAME=hg38_genes
-GENE_LONGER_THAN=5000
-DISTANCE_GREATER_THAN=5000
+declare -i GENE_LONGER_THAN=5000
+declare -i DISTANCE_GREATER_THAN=5000
 
-bedtools closest -a ${GENELIST_NAME}.bed -b ${GENELIST_NAME}.bed -d -N > ${GENELIST_NAME}_closest.bed
-awk '$13 >= DISTANCE_GREATER_THAN' ${GENELIST_NAME}_closest.bed > ${GENELIST_NAME}_far.bed
-awk '$3 - $2 > GENE_LONGER_THAN' ${GENELIST_NAME}_far.bed > ${GENELIST_NAME}_far_and_long.bed
+bedtools closest -a ${GENELIST_NAME}.bed -b ${GENELIST_NAME}.bed -d -N -t first > ${GENELIST_NAME}_closest.bed
+awk -v dist="$DISTANCE_GREATER_THAN" '$13 >= dist' ${GENELIST_NAME}_closest.bed > ${GENELIST_NAME}_far.bed 
+awk -v gene_length="$GENE_LONGER_THAN" '$3 - $2 > gene_length' ${GENELIST_NAME}_far.bed > ${GENELIST_NAME}_far_and_long.bed
 cut -f 1-6 ${GENELIST_NAME}_far_and_long.bed > ${GENELIST_NAME}_selected.bed
+
