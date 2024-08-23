5a3793e65def6094b365f0c23e3abe12a7165b89
Saygin Gulec <50466176+saygingulec@users.noreply.github.com>
2024-08-23 13:08:24 -0400

    Follow BED format
    The input and output is now expected to be in the BED format.

--- helper_scripts/divide_transcripts.py
+++ helper_scripts/divide_transcripts.py
@@ -1,5 +1,26 @@
+"""
+Divides transcripts into 100 (tx_bin_count) bins and add 2 kb (up_length) of 
+up- and downstream sequences divided into 25 (up_bin_count) bins of length 80 bp.
+
+The input and output follows the BED format. The score column is used to denote
+the bin index.
+
+input row
+chr2L	82420	87387	net_NM_080081.3	0	-
+
+output rows from one transcript
+...
+chr2L	252360	252373	Rpp30_NM_001272879.1	26	-
+chr2L	252346	252360	Rpp30_NM_001272879.1	27	-
+chr2L	252333	252346	Rpp30_NM_001272879.1	28	-
+...
+"""
+
 from statistics import mean
 
+# Parameters
+input_gene_list = 'ce11_chosen.bed'
+output_divided_gene_list = 'ce11_150bin.bed'
 up_length = 2000
 up_bin_count = 25
 tx_bin_count = 100
@@ -7,14 +28,14 @@ tx_bin_count = 100
 up_bin_length = int(up_length / up_bin_count)
 total_number_of_bins = tx_bin_count + up_bin_count * 2
 
-with open('ce11_chosen.bed') as f:
+with open(input_gene_list) as f:
     for line in f:
         col = line.strip().split()
         chrom = col[0]
         tx_start = int(col[1])
         tx_end = int(col[2])
-        strand = col[3]
-        tx_name = col[5]
+        tx_name = col[3]
+        strand = col[5]
         tx_length = tx_end - tx_start
 
         # upstream
@@ -52,7 +73,7 @@ with open('ce11_chosen.bed') as f:
             starts.reverse()
             ends.reverse()
 
-        with open('ce11_150bin.bed', 'a') as d:
+        with open(output_divided_gene_list, 'a') as d:
             for i in range(total_number_of_bins):
                 d.write('{chrom}\t{tx_start}\t{tx_end}\t{tx_name}\t{bin_no}\t{strand}\n'.format(
                     chrom=chrom,
